b899e7a9d4b3886b3fa72e70ec8f82be370f9bce
wangpeng <17809267019@163.com>
2019-05-28 08:49:40 +0800

    提交代码王鹏

--- src/main/java/com/xdclass/echo/EchoServer.java
+++ src/main/java/com/xdclass/echo/EchoServer.java
@@ -3,11 +3,16 @@ package com.xdclass.echo;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.sctp.nio.NioSctpServerChannel;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import sun.misc.Unsafe;
+
+import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
 
 public class EchoServer {
 
@@ -17,13 +22,27 @@ public class EchoServer {
         this.port = port;
     }
     public void run() throws InterruptedException {
-            //线程组
-            EventLoopGroup bossGrop = new NioEventLoopGroup();
+
+
+            /*      单线程模型
+                    EventLoopGroup workGrop = new NioEventLoopGroup(1);   //
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(workGrop).channel(NioServerSocketChannel.class).childHandler(new ChannelInitializer<SocketChannel>() {
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                                    socketChannel.pipeline().addLast(new EchoServerHandler());
+                        }
+                    });
+             */
+        //线程组
+            EventLoopGroup bossGrop = new NioEventLoopGroup();   //默认线程数量未cpu核数*2
             EventLoopGroup workGrop = new NioEventLoopGroup();
+
         try {
             ServerBootstrap serverBootstrap = new ServerBootstrap();
             serverBootstrap.group(bossGrop, workGrop)
                     .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG,1024) //io连接的等待队列长度
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         protected void initChannel(SocketChannel socketChannel) throws Exception {
                                     socketChannel.pipeline().addLast(new EchoServerHandler());
@@ -33,8 +52,10 @@ public class EchoServer {
             //绑定端口，同步等待成功
             ChannelFuture channelFuture = serverBootstrap.bind(port).sync();
             //等待服务端监听端口关闭
+            System.out.println("ChannelFuture....===="+channelFuture.get());
             channelFuture.channel().closeFuture().sync();
-        }finally {
+        } catch (ExecutionException e) {
+        } finally {
                 bossGrop.shutdownGracefully();
                 workGrop.shutdownGracefully();
         }
--- src/main/java/com/xdclass/echo/EchoServerHandler.java
+++ src/main/java/com/xdclass/echo/EchoServerHandler.java
@@ -5,15 +5,40 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
 
-import java.nio.charset.Charset;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    private Integer      integer  = 0;
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelRegistered ...... ");
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelUnregistered ...... ");
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+              integer++;
+
+        System.out.println("channelActive ...... "+integer);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+       integer--;
+        System.out.println("channelInactive ...... "+integer);
+    }
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf byteBuf =(ByteBuf) msg;
         System.out.println("服务端接受数据："+byteBuf.toString(CharsetUtil.UTF_8));
-
+        ctx.writeAndFlush("你好啊");
     }
 
     @Override
@@ -27,4 +52,6 @@ public class EchoServerHandler extends ChannelInboundHandlerAdapter {
       cause.printStackTrace();
       ctx.close();
     }
+
+
 }
